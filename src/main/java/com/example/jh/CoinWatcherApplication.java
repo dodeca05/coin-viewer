5c7d4cf79005955bf9e14e1e088108dd8b129f5a
56589 <kim565898@gmail.com>
2024-05-22 23:52:38 +0900

    init commit

---
+++ src/main/java/com/example/jh/CoinWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jh/CoinWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
