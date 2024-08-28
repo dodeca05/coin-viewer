47898d3f017f064dbe0d23e364136f8c66a4de6b
56589 <kim565898@gmail.com>
2024-08-28 23:35:32 +0900

    조회 개수 제한 해제

--- src/main/java/com/example/jh/service/market/Upbit.java
+++ src/main/java/com/example/jh/service/market/Upbit.java
@@ -4,10 +4,12 @@ import com.example.jh.model.CoinPriceModel;
 import com.example.jh.service.Market;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,75 +19,79 @@ import reactor.core.publisher.Mono;
 @Service
 public class Upbit implements Market {
 
-  private final WebClient webClient;
-  private final DateTimeFormatter formatter;
+    private final WebClient webClient;
+    private final DateTimeFormatter formatter;
 
-  public Upbit() {
-    this.webClient = WebClient.create("https://api.upbit.com");
-    this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-  }
+    public Upbit() {
+        this.webClient = WebClient.create("https://api.upbit.com");
+        this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    }
 
-  @SneakyThrows
-  @Override
-  public List<String> getAllCoinList() {
+    @SneakyThrows
+    @Override
+    public List<String> getAllCoinList() {
 
-    String response = webClient.get()
-        .uri("/v1/market/all?isDetails=false")
-        .retrieve()
-        .bodyToMono(String.class).block();
-    List<String> result = new ArrayList<>();
-    ObjectMapper objectMapper = new ObjectMapper();
+        String response = webClient.get()
+                .uri("/v1/market/all?isDetails=false")
+                .retrieve()
+                .bodyToMono(String.class).block();
+        List<String> result = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-    JsonNode jsonNode = objectMapper.readTree(response);
-    if (jsonNode.isArray()) {
-      for (JsonNode node : jsonNode) {
-        JsonNode nameNode = node.get("market");
-        if (nameNode != null) {
-          String coinName = nameNode.asText();
-          if (nameNode.asText().startsWith("KRW")) { //KRW거래 가능한것만 표시하자
-            result.add(coinName);
-          }
+        JsonNode jsonNode = objectMapper.readTree(response);
+        if (jsonNode.isArray()) {
+            for (JsonNode node : jsonNode) {
+                JsonNode nameNode = node.get("market");
+                if (nameNode != null) {
+                    String coinName = nameNode.asText();
+                    if (nameNode.asText().startsWith("KRW")) { //KRW거래 가능한것만 표시하자
+                        result.add(coinName);
+                    }
+                }
+            }
         }
-      }
-    }
-    return result;
+        return result;
 
-  }
-
-  @SneakyThrows
-  @Override
-  public CoinPriceModel[] getCoinPrices(String cointicker, int interval, int length) {
-    String response = webClient.get()
-        .uri("/v1/candles/minutes/" + interval + "?market=" + cointicker + "&count=" + length)
-        .retrieve()
-        .bodyToMono(String.class).block();
-
-    if (length > 200) {
-      length = 200;
     }
 
-    CoinPriceModel[] result = new CoinPriceModel[length];
-    ObjectMapper objectMapper = new ObjectMapper();
-    JsonNode jsonNode = objectMapper.readTree(response);
+    @SneakyThrows
+    @Override
+    public CoinPriceModel[] getCoinPrices(String cointicker, int interval, int length) {
+        CoinPriceModel[] result = new CoinPriceModel[length];
+        ObjectMapper objectMapper = new ObjectMapper();
+        int index = 0;
+        int remainCount=length;
+        while (index < length) {
 
-    int index = 0;
-    if (jsonNode.isArray()) {
-      for (JsonNode node : jsonNode) {
-        JsonNode singleNode = node.get("market");
-        if (singleNode != null) {
-          result[index] = CoinPriceModel.builder()
-              .date(LocalDateTime.parse(node.get("candle_date_time_kst").asText(),formatter))
-              .high(node.get("high_price").asDouble())
-              .low(node.get("low_price").asDouble())
-              .open(node.get("opening_price").asDouble())
-              .close(node.get("trade_price").asDouble())
-              .volume(node.get("candle_acc_trade_volume").asDouble())
-              .build();
-          index++;
+            String url="/v1/candles/minutes/" + interval + "?market=" + cointicker + "&count=" + Math.min(remainCount, 200);
+            if(index!=0)
+              url+="&to="+result[index-1].getDate().format(formatter);
+            String response = webClient.get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(String.class).block();
+            JsonNode jsonNode = objectMapper.readTree(response);
+
+
+            if (jsonNode.isArray()) {
+                for (JsonNode node : jsonNode) {
+                    JsonNode singleNode = node.get("market");
+                    if (singleNode != null) {
+                        result[index] = CoinPriceModel.builder()
+                                .date(LocalDateTime.parse(node.get("candle_date_time_utc").asText(), formatter))
+                                .high(node.get("high_price").asDouble())
+                                .low(node.get("low_price").asDouble())
+                                .open(node.get("opening_price").asDouble())
+                                .close(node.get("trade_price").asDouble())
+                                .volume(node.get("candle_acc_trade_volume").asDouble())
+                                .build();
+                        index++;
+                    }
+                }
+            }
+          remainCount-=200;
         }
-      }
-    }
 
-    return result;
-  }
+        return result;
+    }
 }
